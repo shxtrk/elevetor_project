2aa6de5eff149af838b130ec8f445a956c4a0dfc
shxtrk <direct@shxtrk.dev>
2023-02-21 14:09:47 +0200

    Initial Commit

---
+++ elevator_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elevator_project
+//
+//  Created by Serhii Striuk on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
